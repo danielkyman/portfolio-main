849ecffd92fd510285c21817f5b90b5ccb5fef46
danielkyman <danny.kyman@gmail.com>
2020-08-10 16:36:58 -0700

    added references and message form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
+import Accordion from "react-bootstrap/Accordion";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
@@ -98,6 +101,122 @@ function App() {
               </Row>
             </Col>
           </CardDeck>
+          <Accordion>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="0">
+                <h2>References</h2>
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <blockquote className="blockquote mb-0">
+                    <p>
+                      “Clever rockstar of a coder with a magnetic personality
+                      and the strongest work ethic you will ever find in a
+                      candidate.”
+                    </p>
+                    <footer className="blockquote-footer">
+                      <cite>Team Lead</cite>
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <blockquote className="blockquote mb-0">
+                    <p>
+                      Daniel is someone who always pushes for the best. He would
+                      be the first person I would think to partner with on any
+                      project and he is an ideal team mate.
+                    </p>
+                    <footer className="blockquote-footer">
+                      <cite>Peer / Colleague</cite>
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <blockquote className="blockquote mb-0">
+                    <p>
+                      Daniel embraces challenges instead of avoiding them. When
+                      he worked on our team, he always insisted on doing things
+                      properly. He refused to take the easy way out. I admire
+                      his determination to maintain high standards and never
+                      compromise the project’s integrity.
+                    </p>
+                    <footer className="blockquote-footer">
+                      <cite>Peer / Colleague</cite>
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <blockquote className="blockquote mb-0">
+                    <p>
+                      Daniel is one of the strongest coders that I have met in
+                      my time at Lambda. I think he is a tremendously hard
+                      worker and an asset for any team.
+                    </p>
+                    <footer className="blockquote-footer">
+                      <cite>Peer / Colleague</cite>
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+          <Card>
+            <Card.Header as="h2" className="text-center">
+              Get In Touch
+            </Card.Header>
+            <Card.Body>
+              <Form>
+                <Form.Group as={Row} controlId="formHorizontalName">
+                  <Form.Label column sm={2}>
+                    Name
+                  </Form.Label>
+                  <Col sm={10}>
+                    <Form.Control type="name" placeholder="Name" />
+                  </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} controlId="formHorizontalEmail">
+                  <Form.Label column sm={2}>
+                    Email
+                  </Form.Label>
+                  <Col sm={10}>
+                    <Form.Control type="email" placeholder="Email" />
+                  </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} controlId="formHorizontalMessage">
+                  <Form.Label column sm={2}>
+                    Message
+                  </Form.Label>
+                  <Col sm={10}>
+                    <Form.Control
+                      as="textarea"
+                      type="message"
+                      placeholder="Message"
+                    />
+                  </Col>
+                </Form.Group>
+
+                <Form.Group as={Row}>
+                  <Col sm={{ span: 10, offset: 2 }}>
+                    <Button type="submit">Submit</Button>
+                  </Col>
+                </Form.Group>
+              </Form>
+            </Card.Body>
+          </Card>
         </main>
       </Container>
     </div>
